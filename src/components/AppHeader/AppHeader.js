cc1acd4f3ab24120cf61c659cdae60eea595b2a1
bekdev <asadbeksr@gmail.com>
2021-12-02 01:38:04 +0500

    done: AppHeader

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./AppHeader.css";
 
-export default function AppHeader() {
+export default function AppHeader({ countDone, countAllTodo }) {
   return (
     <div className="app-header ">
       <h1>Todo List</h1>
-      <h2>3 more to do, 0 done</h2>
+      <h2>
+        {countAllTodo} more to do, {countDone} done
+      </h2>
     </div>
   );
 }
