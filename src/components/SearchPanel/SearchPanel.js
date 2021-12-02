2721bb25841c73bfa0473b8c5c5494ad4e9c23fa
bekdev <asadbeksr@gmail.com>
2021-12-02 16:59:07 +0500

    fix: done search panel

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./SearchPanel.css";
+import { useState } from "react";
+export default function SearchPanel({ onSearchChange }) {
+  const [term, setTerm] = useState("");
 
-export default function SearchPanel() {
+  function onSearch(e) {
+    setTerm(e.target.value);
+    onSearchChange(e.target.value);
+  }
+
   return (
     <div className="search-panel d-inline-block mr-2">
       <input
+        type="text"
         className="form-control pl-4 pr-5 py-2"
         placeholder="type to search"
         autoComplete="off"
+        value={term}
+        onChange={onSearch}
       ></input>
     </div>
   );
